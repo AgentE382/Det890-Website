66993dcb762e29a362e53fe0f8144be0ee469c1c
AgentE382 <AgentE382@gmail.com>
2016-05-05 22:27:07 -0400

    Add cadre bio stubs

---
+++ pages/cadre/bujaiski.php
@@ -0,0 +1,31 @@
+<div class="content">
+
+<div class="well pull-right"><img src="img/bujaiski.jpg" /></div>
+
+<h2>Captain Gabriel <!--M.--> Bujaiski: Guest Cadre</h2>
+
+<p>Bio</p>
+
+<h3>Education</h3>
+<p><span class="span1">YYYY</span> Degree, Concentration, University
+<p><span class="span1">YYYY</span> School, Location
+
+<h3>Assignments</h3>
+<ol>
+<li>Mmm YYYY &ndash; Mmm YYYY, Job, Location</li>
+</ol>
+
+<h3>Major Awards and Decorations</h3>
+<ul>
+<li>Important awards, medals, and positions</li>
+</ul>
+
+<h3>Other Achievements</h3>
+<ul>
+<li>Less important awards, medals, and positions</li>
+</ul>
+
+<h3>Effective Dates of Promotion</h3>
+<p><span class="span3">Rank</span> Month D, YYYY</p>
+
+</div><!-- /.content -->
