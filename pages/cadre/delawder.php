0bfeeead1a5a7e5d1e40d684e08c7461ee394592
AgentE382 <AgentE382@gmail.com>
2016-05-13 23:53:39 -0400

    Finish Captain Delawder Bio

--- pages/cadre/delawder.php
+++ pages/cadre/delawder.php
@@ -2,34 +2,67 @@
 
 <div class="well pull-right"><img src="img/delawder.jpg" /></div>
 
-<h2>Captain Colleen M. Delawder: Recruiting Flight Commander</h2>
+<h2>Captain Colleen M. Delawder: Education Flight Commander</h2>
 
-<p>Bio coming soon!</p>
+<p>Capt Colleen M. Delawder is the Education Flight Commander at the Air Force Reserve Officer Training Corps Detachment 890 at the University of Virginia in Charlottesville.  She oversees the education of over 150 cadets located at the University of Virginia, Liberty University, James Madison University, and Piedmont Virginia Community College.</p>
 
-<!--
-
-<p>Bio</p>
+<p>Capt Delawder grew up in Green, Ohio and entered the Air Force in January 1999. She has served as Flight Commander for the 10th Intelligence Squadron and various other Officer in Charge position for many communications work centers to include the largest communications hub in U.S. Air Forces Central Command. She has been stationed at locations in Oklahoma, Oman, Japan, South Korea, Italy, Arizona, Qatar, and Virginia. Before assuming her current position, Capt Delawder served as Deputy Director of Staff for the 480th Intelligence, Surveillance and Reconnaissance (ISR) Wing at Langley Air Force Base, Virginia where she led 334 personnel.</p>
 
 <h3>Education</h3>
-<p><span class="span1">YYYY</span> Degree, Concentration, University
-<p><span class="span1">YYYY</span> School, Location
+<p><span class="span1">1999</span> Communications Technical School, Keesler Air Force Base, Mississippi
+<p><span class="span1">2003</span> John L. Levitow Award, Airman Leadership School, Tinker Air Force Base, Oklahoma
+<p><span class="span1">2003</span> Associate in Applied Science degree in Electronic Systems Technology, Community College of the Air Force
+<p><span class="span1">2003</span> Associate in Arts degree, Rose State College
+<p><span class="span1">2007</span> Bachelor of Science degree in Computer Studies, University Maryland University College
+<p><span class="span1">2009</span> Basic Communications Officer Training, Keesler Air Force Base, Mississippi
+<p><span class="span1">2009</span> Air and Space Basic Course, Residence, Maxwell Air Force Base, Alabama
+<p><span class="span1">2011</span> Master of Science in Technology Management, Information Systems and Services, University Maryland University College
+<p><span class="span1">2014</span> Squadron Officer School, Residence, Maxwell Air Force Base, Alabama
 
 <h3>Assignments</h3>
 <ol>
-<li>Mmm YYYY &ndash; Mmm YYYY, Job, Location</li>
+<li>January 1999 &ndash; February 1999, Trainee, Basic Military Training, Lackland AFB, Texas</li>
+<li>March 1999 &ndash; September 1999, Communications Technical School, Keesler AFB, Mississippi</li>
+<li>September 1999 &ndash; December 2003, Airborne Warning and Control System (AWACS) computer technician, 552nd Component Repair Squadron, Tinker AFB, Oklahoma (March 2002 – May 2002, AWACS computer technician, Thumrait Air Base, Oman; September 2002 – October 2002, AWACS computer technician, Thumrait, Oman; January 2003 – May 2003, AWACS computer technician, Thumrait, Oman)</li>
+<li>December 2003 &ndash; December 2005, AWACS computer NCOIC, 961st Aircraft Maintenance Unit, Kadena AB, Japan</li>
+<li>January 2006 &ndash; October 2006, Quality Assurance Evaluator, 51st Communications Squadron, Osan AB, Republic of Korea</li>
+<li>October 2006 &ndash; September 2008, Network Operations, 31st Communications Squadron, Aviano AB, Italy</li>
+<li>September 2008 &ndash; May 2012, Deputy CC, Plans and Resources Flight, Davis-Monthan AFB, AZ (April 2011 – October 2011, Technical Control Facility Officer in Charge, Al Udeid Air Base, Qatar)</li>
+<li>May 2012 &ndash; January 2015, Deputy Director of Staff, 480th ISR Wing, Langley AFB, VA (May 2012 – June 2014, Flight Commander, 10th Intelligence Squadron, Langley AFB; November 2013 – June 2014, Flight Commander, Warrior Alpha, Al Udeid Air Base, Qatar)</li>
+<li>January 2015 &ndash; Present, Education Flight Commander, Air Force Reserve Officer Training Corps (AFROTC) Detachment 890, VA</li>
 </ol>
 
 <h3>Major Awards and Decorations</h3>
 <ul>
-<li>Important awards, medals, and positions</li>
+<li>Air Force Commendation Medal with five oak leaf clusters</li>
+<li>Air Force Achievement Medal </li>
+<li>Meritorious Unit Award</li>
+<li>Air Force Outstanding Unit Award with Valor and six oak leaf clusters</li>
+<li>National Defense Service Medal</li>
+<li>Global War on Terrorism Expeditionary Medal</li>
+<li>Global War on Terrorism Service Medal</li>
+<li>Korean Defense Service Medal</li>
 </ul>
 
 <h3>Other Achievements</h3>
 <ul>
-<li>Less important awards, medals, and positions</li>
+<li>2004 18th Wing Lieutenant General Leo Marquez Award</li>
+<li>2009 355th Mission Support Group CGO of the Quarter</li>
+<li>2012 355th Mission Support Group CGO of the Quarter</li>
+<li>2012 10th Intelligence Squadron GCO of the Year</li>
+<li>2012 497th Group Information Dominance Program Award</li>
+<li>2012 497th Group Tuskegee Airmen Award</li>
+<li>2012 480th Wing Information Operations Award</li>
+<li>2013 10th Intelligence Squadron Lance P. Sijan Award</li>
+<li>2014 379th Expeditionary Operations Group CGO Warrior of the Quarter</li>
 </ul>
 
 <h3>Effective Dates of Promotion</h3>
-<p><span class="span3">Rank</span> Month D, YYYY</p> -->
+<p><span class="span3">Airman First Class</span> January 13, 1999</p>
+<p><span class="span3">Senior Airman</span> May 13, 2001</p>
+<p><span class="span3">Staff Sergeant</span> October 1, 2003</p>
+<p><span class="span3">Second Lieutenant</span> February 24, 2009</p>
+<p><span class="span3">First Lieutenant</span> February 24, 2011</p>
+<p><span class="span3">Captain</span> February 24, 2013</p>
 
 </div><!-- /.content -->
