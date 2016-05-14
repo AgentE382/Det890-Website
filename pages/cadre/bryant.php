cf550578c3cb089ae8a106e04870ded93848705b
AgentE382 <AgentE382@gmail.com>
2016-05-14 00:08:44 -0400

    Finish TSgt Bryant Bio

--- pages/cadre/bryant.php
+++ pages/cadre/bryant.php
@@ -4,32 +4,53 @@
 
 <h2>TSgt Tinisha L. Bryant: NCOIC</h2>
 
-<p>Bio coming soon!</p>
+<p>TSgt Tinisha L. Bryant is the Non-Commissioned Officer in Charge (NCOIC) of AFROTC Detachment 890, Charlottesville, VA.  She is responsible for the developing and mentoring junior NCO cadre and cadets.</p>
 
-<!--
-
-<p>Bio</p>
+<p>TSgt Bryant grew up in Portsmouth, Virginia and entered the Air Force in May 1998. She has served as Non-Commissioned Officer in Charge for three Commander Support Staffs (CSS). She has been stationed at locations in Virginia, Germany, Florida and Georgia to include a deployment to Saudi Arabia and CENTCOM. Before assuming her current position, TSgt Bryant served as Non-Commissioned Officer in Charge, 15th Air Support Operations Squadron (ASOS) at Fort Stewart Army Installation, Hinesville, GA where she was responsible for 150 personnel.</p>
 
 <h3>Education</h3>
-<p><span class="span1">YYYY</span> Degree, Concentration, University
-<p><span class="span1">YYYY</span> School, Location
+<p><span class="span1">1998</span> Information Management Technical School, Keesler Air Force Base, Mississippi
+<p><span class="span1">2002</span> Personnel Apprentice Course, Keesler Air Force Base, Mississippi
+<p><span class="span1">2003</span> Airman Leadership School, Langley AFB, VA
+<p><span class="span1">2009</span> Associates in Human Resource Management, Community College of the Air Force
+<p><span class="span1">2012</span> Senior Enlisted Joint Professional Military Education (Non Residence), Joint Force Staff College, VA
+<p><span class="span1">2013</span> Non-Commissioned Officer Academy, Knoxville, Tennessee
 
 <h3>Assignments</h3>
 <ol>
-<li>Mmm YYYY &ndash; Mmm YYYY, Job, Location</li>
+<li>May 1998 &ndash; Jun 1998, Trainee, Basic Military Training, Lackland AFB, Texas</li>
+<li>Jun 1998 &ndash; August 1998, Information Manager Technical School, Keesler AFB, Mississippi, November 2001-Dec 2001, Personnel Technical School, Keesler AFB, Mississippi</li>
+<li>August 1998 &ndash; May 2000, Flight Information Apprentice, 1st Transportation Squadron, Langley AFB, VA, May 2000 – November 2001, Fight Information Manager, 1st Transportation Squadron, Langley AFB, VA, November 2001 - January 2002, Force Protection Escort, 363rd Expeditionary Civil Engineer Squadron, Prince Sultan AB, Kingdom of Saudi Arabia (OPERATION SOUTHERN WATCH), January 2002 – May 2003, Command Records Assistant, HQ ACC Langley AFB, VA</li>
+<li>May 2003 &ndash; June 2005, NCOIC, Inbound Assignments, European Mission Support Squadron, Stuttgart-Vaihingen, Germany )</li>
+<li>Jun 2005 &ndash; Jun 2006, Assistant NCOIC Customer Service, 6th Mission Support Squadron, June 2006 – July 2007, NCOIC, Commander Support Staff, 6th Operations Group, July 2007 – September 2011, Customer Service Supervisor, 6th Force Support Squadron, MacDill AFB, Florida</li>
+<li>September 2011 &ndash; July 2015, NCOIC Commander Support Staff, 15th Air Support Operations Squadron, Fort Stewart Georgia</li>
+
 </ol>
 
 <h3>Major Awards and Decorations</h3>
 <ul>
-<li>Important awards, medals, and positions</li>
+<li>Air Force Commendation Medal with two oak leaf clusters</li>
+<li>Joint Service Achievement Medal</li>
+<li>Air Force Achievement Medal</li>
+<li>Meritorious Unit Award with oak leaf cluster</li>
+<li>Air Force Outstanding Unit Award with Valor and six oak leaf clusters</li>
+<li>National Defense Service Medal</li>
+<li>Global War on Terrorism Expeditionary Medal</li>
+<li>Global War on Terrorism Service Medal</li>
+<li>Nuclear Deterrence Operations Service Medal</li>
 </ul>
 
 <h3>Other Achievements</h3>
 <ul>
-<li>Less important awards, medals, and positions</li>
+<li>2010 MacDill Air Force Base, Personnel of the Year</li>
 </ul>
 
 <h3>Effective Dates of Promotion</h3>
-<p><span class="span3">Rank</span> Month D, YYYY</p> -->
+<p><span class="span3">Airman Basic</span> May 2, 1998</p>
+<p><span class="span3">Airman</span> November 21, 1998</p>
+<p><span class="span3">Airman First Class</span> September 21, 1999</p>
+<p><span class="span3">Senior Airman</span> May 21, 2001</p>
+<p><span class="span3">Staff Sergeant</span> September 1, 2004</p>
+<p><span class="span3">Technical Sergeant</span> August 1, 2012</p>
 
 </div><!-- /.content -->
