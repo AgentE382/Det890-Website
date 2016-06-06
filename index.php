74f266ff9371e0feb3740c34cdd9a009b300256a
AgentE382 <AgentE382@gmail.com>
2016-06-06 17:23:36 -0400

    Add Capt Brown to Cadre list

--- index.php
+++ index.php
@@ -57,6 +57,7 @@ $path = $_REQUEST["p"] ? $_REQUEST["p"] : "index";
 			    	<li><a tabindex="-1" href="?p=cadre/hiss">Col Hiss, Commander</a></li>
 			    	<li><a tabindex="-1" href="?p=cadre/rourke">Capt Rourke, OFC</a></li>
 			    	<li><a tabindex="-1" href="?p=cadre/delawder">Capt Delawder, EFC</a></li>
+			    	<li><a tabindex="-1" href="?p=cadre/brown">Capt Brown, RFC</a></li>
 			    	<li><a tabindex="-1" href="?p=cadre/bujaiski">Capt Bujaiski, Guest Cadre</a></li>
 			    	<li><a tabindex="-1" href="?p=cadre/bryant">TSgt Bryant, NCOIC</a></li>
 			    	<li><a tabindex="-1" href="?p=cadre/norris">SSgt Norris, NCOIC Administration</a></li>
