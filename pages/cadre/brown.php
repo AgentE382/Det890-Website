c8a5d6d8dd616dca7f58f6c6c4b3b4e0407eeebb
AgentE382 <AgentE382@gmail.com>
2016-06-06 17:34:21 -0400

    Add Capt Brown bio

---
+++ pages/cadre/brown.php
@@ -0,0 +1,38 @@
+<div class="content">
+
+<div class="well pull-right"><img src="img/brown.jpg" /></div>
+
+<h2>Captain Daniel C. Brown: Recruiting Flight Commander</h2>
+
+<p>Capt Daniel C. Brown is the Recruiting Flight Commander, Air Force Reserve Officer Training Corps Detachment 890, University of Virginia, Charlottesville. He is responsible for the development, management, &amp; execution of all unit recruiting efforts at the University of Virginia, Liberty University, James Madison University, and Piedmont Virginia Community College.
+
+Capt Brown was commissioned in 2010 as a graduate of the Air Force Reserve Officer Training Corps Reserve Officers’ Training Corps program at the University of Virginia.  He joined the Air Force Reserves in October 2015, and has served at both the National Geospatial-Intelligence Agency and National Security Agency, and was a command briefer and intelligence analyst at the 609th Combined Air Operations Center. Prior to his current position, the captain was Intelligence Officer in Charge, Analysis, Correlation, and Fusion Cell, 710th Combat Operations Squadron, Joint Base Langley-Eustis, VA.</p>
+
+<h3>Education</h3>
+<p><span class="span1">2010</span> Bachelor of Arts degree in foreign affairs, University of Virginia, Charlottesville
+<p><span class="span1">2010</span> Air and Space Basic Course, Maxwell Air Force Base (AFB), Ala.
+<p><span class="span1">2011</span> Intelligence Officer Course, Goodfellow AFB, San Angelo, Texas
+<p><span class="span1">2014</span> Master of Intelligence Studies, American Military University, Charles Town, W. Va.
+<p><span class="span1">2015</span> Squadron Officer School, Maxwell AFB, Ala.
+
+<h3>Assignments</h3>
+<ol>
+<li>November 2010 &ndash; September 2011, Student, Intelligence Officer Course, Goodfellow AFB, Texas</li>
+<li>October 2011 &ndash; February 2012, Deputy Officer in Charge, Counter-Insurgency desk, Al Udeid Air Base (AB), Qatar</li>
+<li>February 2012 &ndash; October 2012, Intelligence Analyst, Political, Economic, Communications, Command and Control desk, Al Udeid AB, Qatar</li>
+<li>October 2012 &ndash; September 2014, Cryptologic Analyst, Air Force Cryptologic Office, AF ISR Agency, Ft Meade, Md.</li>
+<li>October 2014 &ndash; September 2015, Congressional Affairs Officer, National Geospatial-Intelligence Agency, Ft Belvoir, Va.</li>
+<li>October 2015 &ndash; May 2015, Intelligence Officer in Charge, 710th Combat Operations Squadron, Joint Base Langley-Eustis, Va.</li>
+</ol>
+
+<h3>Major Awards and Decorations</h3>
+<ul>
+<li>Air Force Commendation Medal with oak leaf cluster</li>
+</ul>
+
+<h3>Effective Dates of Promotion</h3>
+<p><span class="span3">Second Lieutenant</span> May 22, 2010</p>
+<p><span class="span3">First Lieutenant</span> August 1, 2012</p>
+<p><span class="span3">Captain</span> August 1, 2014</p>
+
+</div><!-- /.content -->
